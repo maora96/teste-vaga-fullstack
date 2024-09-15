64a57c9b746d8b51ef99c1ecf2bd9fcf8c997255
maora <marinaoliveira.r07@gmail.com>
2024-09-15 16:15:42 -0300

    feat: formatted data csv

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Row } from "./common/types.ts";
 import { formatMoney, isDocumentValid } from "./common/utils.ts";
 import csv from "csv-parser";
 import fs from "fs";
+import { stringify } from "csv-stringify";
 
 const readFile = async (): Promise<Row[]> => {
   const results: Row[] = [];
@@ -21,20 +22,33 @@ const readFile = async (): Promise<Row[]> => {
 
 const start = async () => {
   const array = await readFile();
-  const formattedMoneyArray = array.map((row: Row) => ({
-    ...row,
-    vlTotal: formatMoney(parseFloat(row.vlTotal)),
-    vlPresta: formatMoney(parseFloat(row.vlPresta)),
-    vlMora: formatMoney(parseFloat(row.vlMora)),
-    vlMulta: formatMoney(parseFloat(row.vlMulta)),
-    vlOutAcr: formatMoney(parseFloat(row.vlOutAcr)),
-    vlIof: formatMoney(parseFloat(row.vlIof)),
-    vlDescon: formatMoney(parseFloat(row.vlDescon)),
-    vlAtual: formatMoney(parseFloat(row.vlAtual)),
-    nrCpfCnpj: isDocumentValid(row.nrCpfCnpj) ? row.nrCpfCnpj : "Não válido.",
-  }));
+  const formattedArray = array.map((row: Row) => {
+    const installmentValue =
+      parseFloat(row.vlTotal) / parseFloat(row.qtPrestacoes);
+    return {
+      ...row,
+      vlTotal: formatMoney(parseFloat(row.vlTotal)),
+      vlPresta:
+        parseFloat(installmentValue.toFixed(2)) === parseFloat(row.vlPresta)
+          ? formatMoney(parseFloat(row.vlPresta))
+          : formatMoney(parseFloat(installmentValue.toFixed(2))),
+      vlMora: formatMoney(parseFloat(row.vlMora)),
+      vlMulta: formatMoney(parseFloat(row.vlMulta)),
+      vlOutAcr: formatMoney(parseFloat(row.vlOutAcr)),
+      vlIof: formatMoney(parseFloat(row.vlIof)),
+      vlDescon: formatMoney(parseFloat(row.vlDescon)),
+      vlAtual: formatMoney(parseFloat(row.vlAtual)),
+      nrCpfCnpj: isDocumentValid(row.nrCpfCnpj) ? row.nrCpfCnpj : "Não válido.",
+    };
+  });
 
-  console.log(formattedMoneyArray);
+  stringify(formattedArray, { header: true }, (err, output) => {
+    if (err) throw err;
+    fs.writeFile(`src/data/formattedData.csv`, output, "utf8", (err) => {
+      if (err) throw err;
+      console.log(`src/data/formattedData.csv.`);
+    });
+  });
 };
 
-//start();
+start();
